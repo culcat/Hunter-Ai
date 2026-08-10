8bbedb98d1b84441b34b215ab63799f59862f22f
culcat <krainukovaleksandr1@gmail.com>
2026-08-10 15:15:17 +0300

    fix: configure outputFileTracingRoot in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   transpilePackages: ['@hunter-ai/types', 'antd', '@ant-design/nextjs-registry'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
